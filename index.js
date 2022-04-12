ad7381df815262af221f3498d0cfe25e6855123e
Cristhian <https://github.com/cristhianrave>
2022-04-13 01:46:14 +0200

    push

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 $(function () {
+  $(".btn-restart").hide();
+
   /* Ancho de la ventana */
   var windowWidth = $(window).width();
-  /* Longitud de la pista */
-  var raceLenght = windowWidth - 100;
+  console.log(windowWidth);
 
-  var h; 
-  
+  /* Tamaño del coche */
+  var carWidth = $(".car").width();
+
+  /* Longitud de la pista */
+  var raceLenght = windowWidth - carWidth * 2;;
+
+  /* Meta final */
+  var finalRace = raceLenght - carWidth;
+
   /* Array con las imgs de los coches */
   var coches = [
     $("#rail1").hide(),
@@ -18,31 +26,76 @@ $(function () {
     $("#rail8").hide(),
     $("#rail9").hide(),
   ];
-  
+
+  /* Boton inicio carrera */
   $(".btn-start").on("click", function () {
+    var speedsCars = [{}];
+    var car;
+    var moveCar;
+    var speedCar = {};
+
+    $(".btn-start").hide();
+    $(".btn-restart").show();
+
     /* cantidad de jugadores seleccionados por pantalla */
     var option = $("#players option:selected").val();
 
-    
     /* cantidad de coches a mostrar  */
     for (let index = 0; index < option; index++) {
-      
-      /* Valor random de la velocidad de los coches */
-      var raceSpeed = Math.floor(Math.random() * 5000 + 1);
 
-      h = coches[index];
-      h.show();
+      /* Valor random de la velocidad de los coches */
+      var speed = Math.random() * (10 - 1) + 1; //cambiar valor
+      var speedRace = Math.round(speed) * 1000;
+
+      car = coches[index];
+      car.show();
 
       /* Animacion de los coches */
-      $("#car" + (index + 1)).animate(
-        {left: raceLenght},raceSpeed);
-    } 
+      moveCar = $("#car" + (index + 1)).animate(
+        { left: raceLenght },
+        speedRace
+      );
+      console.log(raceLenght);
+
+      /* Agregar coche y su velocidad al array */
+      speedCar = { moveCar, speedRace };
+      speedsCars.push(speedCar);
+    }
+    /* Eliminar el primer elemento de el array */
+    speedsCars.shift();
+    
+    /* Orden de llegada de los coches  */
+    speedsCars.sort((a, b) => (a.speedRace > b.speedRace ? 1 : -1));
+    console.log(speedsCars);
+    var key;
+    for (key in speedsCars) {
+      if (key == 0) {
+        key = "primero";
+      } else if (key == 1) {
+        key = "segundo";
+      } else if (key == 2) {
+        key = "tercero";
+      } else if (key == 3) {
+        key = "cuarto";
+      } else if (key == 4) {
+        key = "quinto";
+      } else if (key == 5) {
+        key = "sexto";
+      } else if (key == 6) {
+        key = "septimo";
+      } else if (key == 7) {
+        key = "octavo";
+      } else if (key == 8) {
+        key = "noveno";
+      }
+      console.log(key);
+    }
 
   });
 
-
   $(".btn-restart").on("click", function () {
-      $(".car").css("left", "0");
-    
+    $(".btn-restart").hide();
+    $(".btn-start").show();
+    $(".car").css("left", "0");
   });
 });
